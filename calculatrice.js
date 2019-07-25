691a353c283f03651b214f15062da1e0301646a2
Bocquet <allan1243@outlook.fr>
2019-07-25 17:11:12 +0200

    first commit

--- calculatrice.js
+++ calculatrice.js
@@ -1,4 +1,25 @@
-let bouton = document.getElementsByClassName("bouton");
-console.log(bouton);
 
-bouton.addEventListener("click", console.log(bouton));
+let bouton = document.getElementsByClassName("bouton");
+let boutn = document.getElementsByClassName("boutn");
+let egal = document.getElementsByClassName("egal")
+let result =  document.getElementById("result")
+for (let i = 0; i < bouton.length; i++) {
+    bouton[i].addEventListener("click",affichage);
+    }
+
+for (let i = 0; i < boutn.length; i++) {
+    boutn[i].addEventListener("click",affichage);
+}
+
+for (let i = 0; i < egal.length; i++) {
+    egal[i].addEventListener("click",afficherResult);
+    
+}
+    function afficherResult() {
+        document.getElementById("result").innerHTML = (eval(result.textContent));
+    }
+
+    function affichage() {
+        document.getElementById("result").innerHTML += this.value;
+    }
+
